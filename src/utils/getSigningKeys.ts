ccc4d993a63a0c4d391353572ac6562d6d3b2002
Sergey Avdyakov <sergeyavdyakov@gmail.com>
2024-12-12 20:21:38 +0400

    Get keys in parallel

--- src/utils/getSigningKeys.ts
+++ src/utils/getSigningKeys.ts
@@ -1,25 +1,34 @@
 import { noRegistryContract } from "../contracts/noRegistryContract";
 
+async function getOperatorKeys(operatorId: bigint, blockNumber: bigint) {
+  const limit = await noRegistryContract.read.getTotalSigningKeyCount(
+    [operatorId],
+    { blockNumber }
+  );
+  let signingKeys: any[] = [];
+  try {
+    signingKeys = (await noRegistryContract.read.getSigningKeys(
+      [operatorId, 0, limit],
+      { blockNumber }
+    )) as any[];
+  } catch (e) {}
+  return signingKeys;
+}
+
 export async function getSigningKeys({ blockNumber }: { blockNumber: bigint }) {
   const nodeOperatorsCount =
     await noRegistryContract.read.getNodeOperatorsCount();
-  const operatorIds = [...Array(Number(nodeOperatorsCount)).keys()];
+  const operatorIds = Array.from(
+    { length: Number(nodeOperatorsCount) },
+    (_, i) => BigInt(i)
+  );
 
   const keys: any[] = [];
-  for (const operatorId of operatorIds) {
-    const limit = await noRegistryContract.read.getTotalSigningKeyCount(
-      [operatorId],
-      { blockNumber }
-    );
-    let signingKeys: any[] = [];
-    try {
-      signingKeys = (await noRegistryContract.read.getSigningKeys(
-        [operatorId, 0, limit],
-        { blockNumber }
-      )) as any[];
-      keys.push(...signingKeys);
-    } catch (e) {}
-  }
 
+  const fetchedKeys = await Promise.all(
+    operatorIds.map((operatorId) => getOperatorKeys(operatorId, blockNumber))
+  );
+
+  keys.push(...fetchedKeys.flat());
   return keys;
 }
