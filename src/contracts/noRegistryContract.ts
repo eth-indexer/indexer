cd26872152dff9c0cd92f829d148cb6f0d0a4958
Sergey Avdyakov <sergeyavdyakov@gmail.com>
2024-12-18 15:38:02 +0400

    Move contract address to env

--- src/contracts/noRegistryContract.ts
+++ src/contracts/noRegistryContract.ts
@@ -2,8 +2,11 @@ import { getContract } from "viem";
 import noRegistryABI from "./noRegistryABI";
 import { publicClient } from "../rpc/publicClient";
 
+const CONTRACT_ADDRESS =
+  process.env.CONTRACT_ADDRESS || "0x595F64Ddc3856a3b5Ff4f4CC1d1fb4B46cFd2bAC";
+
 export const noRegistryContract = getContract({
-  address: "0x595F64Ddc3856a3b5Ff4f4CC1d1fb4B46cFd2bAC",
+  address: CONTRACT_ADDRESS as `0x${string}`,
   abi: noRegistryABI,
   client: publicClient,
 });
