0f1742b6e5c159ae3200969af39496a5d88951a9
Sergey Avdyakov <sergeyavdyakov@gmail.com>
2024-12-18 13:50:27 +0400

    Change types

--- src/blockWatcher.ts
+++ src/blockWatcher.ts
@@ -5,12 +5,12 @@ const FINALIZED_BLOCKS_TO_KEEP = process.env.FINALIZED_BLOCKS_TO_KEEP || 11;
 
 type TWatchingParams = {
   onChange?(blocks: Block[], isReorg?: boolean): void;
-  onCutOffFinalizedBlocks?(blocksToRemove: BigInt[]): void;
+  onCutOffFinalizedBlocks?(blocksToRemove: bigint[]): void;
 };
 
 interface IBlockWatcher {
   blocksStack: Block[];
-  lastFinalizedBlockNumber: BigInt;
+  lastFinalizedBlockNumber: bigint;
   reorgInProgress: boolean;
   startWatching(options?: TWatchingParams): void;
   getCurrentState(): { latestBlocks: Block[]; finalizedBlocks: Block[] };
@@ -21,7 +21,7 @@ export class BlockWatcher implements IBlockWatcher {
   lastFinalizedBlockNumber = 0n;
   reorgInProgress = false;
   onChange?: (blocks: Block[], isReorg?: boolean) => void;
-  onCutOffFinalizedBlocks?: (blocksToRemove: BigInt[]) => void;
+  onCutOffFinalizedBlocks?: (blocksToRemove: bigint[]) => void;
 
   constructor(options?: TWatchingParams) {
     this.onChange = options?.onChange;
@@ -36,7 +36,7 @@ export class BlockWatcher implements IBlockWatcher {
     console.log("Watching blocks...");
     publicClient.watchBlocks({
       onBlock: async (block) => {
-        this.handleNewBlock(block);
+        await this.handleNewBlock(block);
         this.onChange && this.onChange([block]);
       },
     });
@@ -58,121 +58,142 @@ export class BlockWatcher implements IBlockWatcher {
   }
 
   private async coldStart() {
-    const finalizedBlock = await publicClient.getBlock({
-      blockTag: "finalized",
-    });
-    const latestBlock = await publicClient.getBlock({
-      blockTag: "latest",
-    });
-
-    if (!finalizedBlock.number || !latestBlock.number) return;
-
-    this.lastFinalizedBlockNumber = finalizedBlock.number;
-    const startBlockNumber =
-      finalizedBlock.number - BigInt(FINALIZED_BLOCKS_TO_KEEP);
-    const stopBlockNumber = latestBlock.number - BigInt(1);
-
-    const blocksToAdd = Array.from(
-      { length: Number(stopBlockNumber - startBlockNumber) + 1 },
-      (_, index) => startBlockNumber + BigInt(index)
-    );
-
-    let tempBlocksStack: Block[] = [];
-    for (const blockNumber of blocksToAdd) {
-      const blockData = await publicClient.getBlock({
-        blockNumber,
+    try {
+      const finalizedBlock = await publicClient.getBlock({
+        blockTag: "finalized",
       });
-      tempBlocksStack.push(blockData);
-    }
-    this.onChange && this.onChange(tempBlocksStack);
+      const latestBlock = await publicClient.getBlock({
+        blockTag: "latest",
+      });
 
-    console.log("Cold start finished!");
-    this.blocksStack = [...tempBlocksStack, ...this.blocksStack];
+      if (!finalizedBlock.number || !latestBlock.number) return;
+
+      this.lastFinalizedBlockNumber = finalizedBlock.number;
+      const startBlockNumber =
+        finalizedBlock.number - BigInt(FINALIZED_BLOCKS_TO_KEEP);
+      const stopBlockNumber = latestBlock.number - 1n;
+
+      const blocksToAdd = Array.from(
+        { length: Number(stopBlockNumber - startBlockNumber) + 1 },
+        (_, index) => startBlockNumber + (BigInt(index) as bigint)
+      );
+
+      let tempBlocksStack: Block[] = [];
+      for (const blockNumber of blocksToAdd) {
+        const blockData = await publicClient.getBlock({
+          blockNumber,
+        });
+        tempBlocksStack.push(blockData);
+      }
+      this.onChange && this.onChange(tempBlocksStack);
+
+      console.log("Cold start finished!");
+      this.blocksStack = [...tempBlocksStack, ...this.blocksStack];
+    } catch (error) {
+      console.error("Error during cold start:", error);
+    }
   }
 
   private async handleNewBlock(block: Block) {
-    console.log("New block: ", block.number);
-    const newBlockNumber = block.number || 0;
-    const previousBlockNumber = this.blocksStack.length
-      ? this.blocksStack[this.blocksStack.length - 1].number || 0
-      : 0;
-    if (!this.blocksStack.length || newBlockNumber > previousBlockNumber) {
-      this.blocksStack.push(block);
-      // this.sliceFinalizedBlocks();
-      this.checkReorg();
+    try {
+      console.log("New block: ", block.number);
+      const newBlockNumber = block.number || 0n;
+      const previousBlockNumber = this.blocksStack.length
+        ? this.blocksStack[this.blocksStack.length - 1].number || 0n
+        : 0n;
+      if (!this.blocksStack.length || newBlockNumber > previousBlockNumber) {
+        this.blocksStack.push(block);
+        // this.sliceFinalizedBlocks();
+        await this.checkReorg();
+      }
+    } catch (error) {
+      console.error("Error handling new block:", error);
     }
   }
 
   private async sliceFinalizedBlocks() {
-    const lastFinalizedBlock = await publicClient.getBlock({
-      blockTag: "finalized",
-    });
-    if (
-      !this.lastFinalizedBlockNumber ||
-      this.lastFinalizedBlockNumber === lastFinalizedBlock.number
-    )
-      return;
+    try {
+      const lastFinalizedBlock = await publicClient.getBlock({
+        blockTag: "finalized",
+      });
+      if (
+        !this.lastFinalizedBlockNumber ||
+        this.lastFinalizedBlockNumber === lastFinalizedBlock.number
+      )
+        return;
 
-    this.lastFinalizedBlockNumber = lastFinalizedBlock.number;
-    const blocksToRemove = this.blocksStack
-      .filter(
+      this.lastFinalizedBlockNumber = lastFinalizedBlock.number;
+      const blocksToRemove = this.blocksStack
+        .filter(
+          (block) =>
+            block.number &&
+            block.number <
+              this.lastFinalizedBlockNumber - BigInt(FINALIZED_BLOCKS_TO_KEEP)
+        )
+        .map((block) => block.number) as bigint[];
+
+      this.blocksStack = this.blocksStack.filter(
         (block) =>
           block.number &&
-          block.number <
+          block.number >=
             this.lastFinalizedBlockNumber - BigInt(FINALIZED_BLOCKS_TO_KEEP)
-      )
-      .map((block) => block.number) as BigInt[];
+      );
 
-    this.blocksStack = this.blocksStack.filter(
-      (block) =>
-        block.number &&
-        block.number >=
-          this.lastFinalizedBlockNumber - BigInt(FINALIZED_BLOCKS_TO_KEEP)
-    );
-
-    this.onCutOffFinalizedBlocks &&
-      this.onCutOffFinalizedBlocks(blocksToRemove);
+      this.onCutOffFinalizedBlocks &&
+        this.onCutOffFinalizedBlocks(blocksToRemove);
+    } catch (error) {
+      console.error("Error slicing finalized blocks:", error);
+    }
   }
 
-  private checkReorg() {
-    const latestBlock = this.blocksStack[this.blocksStack.length - 1];
-    const parentBlock = this.blocksStack[this.blocksStack.length - 2];
+  private async checkReorg() {
+    try {
+      const latestBlock = this.blocksStack[this.blocksStack.length - 1];
+      const parentBlock = this.blocksStack[this.blocksStack.length - 2];
 
-    if (
-      latestBlock &&
-      parentBlock &&
-      latestBlock.parentHash !== parentBlock.hash
-    ) {
-      this.makeBlocksReorg();
+      if (
+        latestBlock &&
+        parentBlock &&
+        latestBlock.parentHash !== parentBlock.hash
+      ) {
+        await this.makeBlocksReorg();
+      }
+    } catch (error) {
+      console.error("Error checking reorg:", error);
     }
   }
 
   private async makeBlocksReorg() {
-    console.log("Making reorg...");
-    this.reorgInProgress = true;
-    const revertedBlocks = this.blocksStack
-      .slice(0, this.blocksStack.length - 1)
-      .reverse();
+    try {
+      console.log("Making reorg...");
+      this.reorgInProgress = true;
+      const revertedBlocks = this.blocksStack
+        .slice(0, this.blocksStack.length - 1)
+        .reverse();
 
-    const reorgonizedBlocks = [];
+      const reorgonizedBlocks = [];
 
-    for (const block of revertedBlocks) {
-      const blockNumber = block.number;
-      if (!blockNumber) continue;
-      const blockchainBlock = await publicClient.getBlock({
-        blockNumber,
-      });
-      if (blockchainBlock.hash !== block.hash) {
-        const blockIndex = this.blocksStack.findIndex(
-          (b) => b.number === block.number
-        );
-        this.blocksStack[blockIndex] = blockchainBlock;
-        reorgonizedBlocks.push(blockchainBlock);
-      } else {
-        break;
+      for (const block of revertedBlocks) {
+        const blockNumber = block.number;
+        if (!blockNumber) continue;
+        const blockchainBlock = await publicClient.getBlock({
+          blockNumber,
+        });
+        if (blockchainBlock.hash !== block.hash) {
+          const blockIndex = this.blocksStack.findIndex(
+            (b) => b.number === block.number
+          );
+          this.blocksStack[blockIndex] = blockchainBlock;
+          reorgonizedBlocks.push(blockchainBlock);
+        } else {
+          break;
+        }
       }
+      this.reorgInProgress = false;
+      this.onChange && this.onChange(reorgonizedBlocks.reverse(), true);
+    } catch (error) {
+      console.error("Error making reorg:", error);
+      this.reorgInProgress = false;
     }
-    this.reorgInProgress = false;
-    this.onChange && this.onChange(reorgonizedBlocks.reverse(), true);
   }
 }
